c70dbdd2ec3ba83589b697a4281bcd94beae4224
danielzyss <d.zyss15@ejm.org>
2017-03-09 17:34:25 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pickle
+import numpy as np
+import pandas as pd
+from matlabTranslator import *
+
+#data import
+ADeyesclosed, ADeyesopened, CNTLeyesclosed, CNTLeyesopened = loadDataFrames()
+
+if __name__ == '__main__':
+
+    print('Nothing yet!')
---
+++ matlabTranslator.py
@@ -0,0 +1,41 @@
+import pickle
+import numpy as np
+import pandas as pd
+import scipy.io as io
+
+
+def MatlabStructure2PandasDataframe(fileaddress, dictionnaryname, outputname):
+
+    ADdata = io.loadmat(fileaddress)
+
+    ADdict = ADdata[dictionnaryname]
+    newDFeyesclosed = pd.DataFrame(columns=['electrode ' + str(i) for i in range(0, 64)])
+    newDFeyesopened = pd.DataFrame(columns=['electrode ' + str(i) for i in range(0, 64)])
+
+    for i in range(0, ADdict.shape[1]):
+        patient = ADdict[0, i]
+
+        # EYES CLOSED :
+        newrow = []
+        for j in range(0, 64):
+            newrow.append(np.array(patient[0][j, :]))
+        newDFeyesclosed.loc[i] = newrow
+
+        # EYES OPENED :
+        newrow = []
+        for j in range(0, 64):
+            newrow.append(np.array(patient[1][j, :]))
+        newDFeyesopened.loc[i] = newrow
+
+    newDFeyesclosed.to_pickle('data/' + outputname + 'eyesclosed.pk')
+    newDFeyesopened.to_pickle('data/' + outputname + 'eyesopened.pk')
+
+
+def loadDataFrames():
+
+    ADeyesclosed = pd.read_pickle('data/ADeyesclosed.pk')
+    ADeyesopened = pd.read_pickle('data/ADeyesopened.pk')
+    CONTROLeyesclosed = pd.read_pickle('data/CONTROLeyesclosed.pk')
+    CONTROLeyesopened = pd.read_pickle('data/CONTROLeyesopened.pk')
+
+    return ADeyesclosed, ADeyesopened , CONTROLeyesclosed, CONTROLeyesopened
