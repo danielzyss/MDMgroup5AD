a5a8d1da71d63c122412cc3f699d2ac534b0ea61
danielzyss <d.zyss15@ejm.org>
2017-03-13 00:48:08 +0000

    added the tools.py file containing modular functions. added steps in main.py file.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from matlabTranslator import *
 from tools import *
 
 
-#data import
+
+
 print('Importing Data.. ')
 ADeyesclosed, ADeyesopened, CNTLeyesclosed, CNTLeyesopened = loadDataFrames()
 
@@ -13,10 +14,25 @@ if __name__ == '__main__':
 
     print('Smoothing..')
     ADeyesclosedSmoothed = smoothDataFrame(ADeyesclosed)
+
     print('Creating Correlation Matrix..')
-    correlMat = correlationDataFrame(ADeyesclosedSmoothed)
-    print('Plotting Correlation Matrix for Patient 0')
-    plotcorrelation(pd.DataFrame(correlMat[0]))
+    correlMatArray = correlationDataFrame(ADeyesclosedSmoothed.head())
+
+    # print('Plotting Correlation Matrix for Patient 0')
+    # plotcorrelation(pd.DataFrame(correlMatArray[0]))
+
+    print('Creating Graphs..')
+    graphArray = correlationmatrix2graph(correlMatArray)
+
+    print('Minimum Spanning Tree..')
+    mstArray = graph2minimumspanningtree(graphArray)
+
+    print('Dendrawing.. ')
+    dendroarray = mst2dendrogram(graphArray)
+
+
+
+
 
 
 
--- tools.py
+++ tools.py
@@ -5,7 +5,12 @@ import sys
 import scipy.signal as signal
 import scipy.stats as stats
 import seaborn as sns
-
+import networkx as nx
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering
+import fastdtw
+from sklearn.preprocessing import normalize
+from scipy.spatial.distance import euclidean
 
 def smoothDataFrame(dataframe):
 
@@ -29,18 +34,55 @@ def correlationDataFrame(dataframe):
     correlation = []
 
     for i in range(0, dataframe.shape[0]):
+
         correlMatrix = np.zeros(shape=(len(dataframe.columns), len(dataframe.columns)))
 
         for j, n in enumerate(dataframe.columns):
 
             for k, m in enumerate(dataframe.columns):
 
-                c = stats.pearsonr(dataframe[n][i], dataframe[m][i])[0]
+                c = abs(stats.pearsonr(dataframe[n][i], dataframe[m][i])[0])
                 correlMatrix[j,k] = c
 
+        correlMatrix = normalize(correlMatrix)
         correlation.append(correlMatrix)
 
     return np.array(correlation)
+
+
+def correlationmatrix2graph(correlmatrixarray):
+    grapharray = []
+
+    for correlmatrix in correlmatrixarray:
+        gr = nx.from_numpy_matrix(correlmatrix, parallel_edges=False)
+        grapharray.append(gr)
+
+    return grapharray
+
+
+def graph2minimumspanningtree(grapharray):
+
+    minimumspanningtreearray = []
+
+    for graph in grapharray:
+        mst = nx.minimum_spanning_tree(graph)
+        minimumspanningtreearray.append(mst)
+
+    return minimumspanningtreearray
+
+
+def mst2dendrogram(mstarray):
+
+    clusteringarray = []
+
+    for mst in mstarray:
+        distance = nx.floyd_warshall_numpy(mst)
+        linkagematrix = linkage(distance)
+        dendro = dendrogram(linkagematrix)
+        clusteringarray.append(dendro)
+        plt.show()
+
+    return clusteringarray
 
 
 def plotcorrelation(correlationarray):
